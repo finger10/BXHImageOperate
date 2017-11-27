510dcfab8e8a3b2e5bfcbe48f216372e3e3e264d
guoguo <1686147171@qq.com>
2017-11-27 19:15:14 +0800

    Initial Commit

---
+++ BXHImageOperate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BXHImageOperate
+//
+//  Created by gg on 2017/11/27.
+//  Copyright © 2017年 JayS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
